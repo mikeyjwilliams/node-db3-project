c38d020667cb58605d85345378b90499f0dc84b6
Michael Williams <manicdesigner@gmail.com>
2020-03-03 08:41:58 -0500

    findSteps function added

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,6 +3,7 @@ const db = require('../data/config');
 module.exports = {
   find,
   findById,
+  findSteps,
 };
 
 function find() {
@@ -13,4 +14,16 @@ function findById(id) {
   return db('schemes')
     .where({ id: id })
     .select('*');
+}
+
+function findSteps(id) {
+  return db('schemes as s')
+    .join('steps as st', 'st.scheme_id', 's.id')
+    .where('s.id', id)
+    .orderBy('st.step_number', 'asc')
+    .select(
+      's.scheme_name as scheme_name',
+      'st.step_number as step_number',
+      'st.instructions as step_instruction'
+    );
 }
