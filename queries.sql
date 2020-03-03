c6b044637750dc3a113444a24eb0edf0f6da4a13
Michael Williams <manicdesigner@gmail.com>
2020-03-02 23:05:36 -0500

    queriy 3 completed

--- queries.sql
+++ queries.sql
@@ -19,4 +19,12 @@ WHERE o."OrderDate" < "2012-08-09";
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+SELECT p."ProductName" AS "Product",
+o."quantity" AS "quantity"
+ FROM "OrderDetail" AS "o"
+ JOIN "Product" as "p"
+ ON o."ProductId" = p."Id"
+ WHERE o."OrderId" = 10251
+ ORDER BY p."ProductName";
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
