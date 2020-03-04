e7d04c15fd22548df0bf665c3b007835774308a8
Mikey Williams <manicdesigner@gmail.com>
2020-03-03 21:18:22 -0500

    stretch query 1

--- queries.sql
+++ queries.sql
@@ -1,40 +1,42 @@
 -- Multi-Table Query Practice
-
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
-
-SELECT p."ProductName" AS "ProductName", c."CategoryName" as "CategoryName"
+SELECT
+  p."ProductName" AS "ProductName",
+  c."CategoryName" as "CategoryName"
 FROM "Product" AS "p"
-JOIN "Category" AS "c"
-ON p."CategoryId" = c."Id";
-
+JOIN "Category" AS "c" ON p."CategoryId" = c."Id";
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
-
-SELECT o."Id" AS "Order Id",
-s."CompanyName" AS "Shipper CompanyName"
+SELECT
+  o."Id" AS "Order Id",
+  s."CompanyName" AS "Shipper CompanyName"
 from "Order" AS "o"
-JOIN "Shipper" as "s"
-on o."ShipVia" = s."Id"
-WHERE o."OrderDate" < "2012-08-09";
-
-
+JOIN "Shipper" as "s" on o."ShipVia" = s."Id"
+WHERE
+  o."OrderDate" < "2012-08-09";
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
-
-SELECT p."ProductName" AS "Product",
-o."quantity" AS "quantity"
- FROM "OrderDetail" AS "o"
- JOIN "Product" as "p"
- ON o."ProductId" = p."Id"
- WHERE o."OrderId" = 10251
- ORDER BY p."ProductName";
-
+SELECT
+  p."ProductName" AS "Product",
+  o."quantity" AS "quantity"
+FROM "OrderDetail" AS "o"
+JOIN "Product" as "p" ON o."ProductId" = p."Id"
+WHERE
+  o."OrderId" = 10251
+ORDER BY
+  p."ProductName";
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All
--- columns should be labeled clearly. Displays 16,789 records.
-
-SELECT o."Id" AS "OrderId",
- c."CompanyName" AS "CompanyName",
- e."LastName" AS "EmployeeLastName"
- FROM "Order" AS "o"
- left JOIN "Employee" AS "e"
- ON o."EmployeeId" = e."Id"
- JOIN "Customer" AS "c"
- ON o."CustomerId" = c."Id";
+  -- columns should be labeled clearly. Displays 16,789 records.
+SELECT
+  o."Id" AS "OrderId",
+  c."CompanyName" AS "CompanyName",
+  e."LastName" AS "EmployeeLastName"
+FROM "Order" AS "o"
+left JOIN "Employee" AS "e" ON o."EmployeeId" = e."Id"
+JOIN "Customer" AS "c" ON o."CustomerId" = c."Id";
+-- STRETCH
+SELECT
+  c."CategoryName" as "CategoryName",
+  COUNT(p."ProductID") as "Product Count"
+FROM "Products" as p
+JOIN "Categories" as c ON p."CategoryID" = c."CategoryID"
+GROUP BY
+  c."CategoryName";
